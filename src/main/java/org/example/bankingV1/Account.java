560df428ae62e3df411ba56c37d8dd4b7443b3c3
Casey Echternacht <casechtern@yahoo.com>
2024-08-04 11:45:35 -0600

    regrouped getters and setters

--- src/main/java/org/example/bankingV1/Account.java
+++ src/main/java/org/example/bankingV1/Account.java
@@ -1,2 +1,50 @@
-package org.example.bankingV1;public class Account {
+package org.example.bankingV1;
+
+public class Account {
+	// need to look further into purpose of "private" modifier here
+	private Integer accountNumber;
+	private double balance;
+	private T accountHolder; //benefit of generic here?
+
+	public Account(Integer accountNumber, double balance, T accountHolder) {
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+		this.accountHolder = accountHolder;
+	}
+
+	//getters
+	public Integer getAccountNumber() {
+		return accountNumber;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public T getAccountHolder() {
+		return accountHolder;
+	}
+	//setters
+	public void setAccountNumber(Integer accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public void setAccountHolder(T accountHolder) {
+		this.accountHolder = accountHolder;
+	}
+
+	//business methods
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	public void withdraw(double amount) {
+		if(amount <= balance) balance -= amount;
+		else System.out.println("Insufficient balance");
+	}
+
+	//Override methods
+	@Override
+	public String toString() {
+		return "Account [accountNumber=" + accountNumber + ", balance=" + balance + ", accountHolder=" + accountHolder;
+	}
 }
---
+++ src/main/java/org/example/bankingV1/README.md
@@ -0,0 +1,4 @@
+There are no tickets for this project.
+It is a simple copy/paste from an existing example.
+While I'm not solving anything, this will give me muscle memory and make
+the appearance of getters, setters, and @Override statements more familiar.
