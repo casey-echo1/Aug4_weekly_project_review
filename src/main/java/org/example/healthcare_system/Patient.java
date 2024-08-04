8c430352b8352324e6dabaa697bc7dc06df0a13c
Casey Echternacht <casechtern@yahoo.com>
2024-08-04 13:24:29 -0600

    added field to new class that references another class I have not yet created. gotta live with error codes for a moment

---
+++ src/main/java/org/example/healthcare_system/Patient.java
@@ -0,0 +1,47 @@
+package org.example.healthcare_system;
+
+import java.util.List;
+
+public class Patient {
+	private int patientID;
+	private String patientName;
+	private List<MedicalRecord> medicalHistory;
+
+	public Patient(int patientID, String name, List<MedicalRecord> medicalHistory) {
+		this.patientID = patientID;
+		this.patientName = name;
+		this.medicalHistory = medicalHistory;
+	}
+
+	public int getPatientID() {
+		return patientID;
+	}
+	public String getPatientName() {
+		return patientName;
+	}
+	public List<String> getMedicalHistory() {
+		return medicalHistory;
+	}
+
+	public void setPatientID(int patientID) {
+		this.patientID = patientID;
+	}
+	public void setPatientName(String patientName) {
+		this.patientName = patientName;
+	}
+	public void setMedicalHistory(List<MedicalRecord> medicalHistory) {
+		this.medicalHistory = medicalHistory;
+	}
+
+	public void addMedicalRecord(MedicalRecord record) {
+		this.medicalHistory.add(record);
+	}
+	public void updateMedicalRecord(int index, MedicalRecord newRecord) {
+		this.medicalHistory.set(index, newRecord);
+	}
+	public void getMedicalRecord(int index) {
+		this.medicalHistory.get(index);
+	}
+
+
+}
