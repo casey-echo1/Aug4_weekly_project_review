bad6cb0231138a4db7173a05b2958497c1d5539b
Casey Echternacht <casechtern@yahoo.com>
2024-08-04 12:11:26 -0600

    Copied Transaction class

--- src/main/java/org/example/bankingV1/SavingsAccount.java
+++ src/main/java/org/example/bankingV1/SavingsAccount.java
@@ -1,2 +1,29 @@
-package org.example.bankingV1;public class SavingsAccount {
+package org.example.bankingV1;
+
+public class SavingsAccount extends Account<String> {
+
+	private double interestRate;
+
+	//interest rate belongs to this class, all other fields are within domain of superClass
+	public SavingsAccount(Integer accountNumber, double balance, String accountHolder, double interestRate) {
+		super(accountNumber, balance, accountHolder);
+		this.interestRate = interestRate;
+	}
+
+	public double getInterestRate() {
+		return interestRate;
+	}
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	@Override
+	public String toString() {
+		return "SavingsAccount{" +
+			"accountNumber=" + getAccountNumber() +
+			", balance=" + getBalance() +
+			", accountHolder='" + getAccountHolder() +
+			"interestRate=" + interestRate +
+			'}';
+	}
 }
---
+++ src/main/java/org/example/bankingV1/Transaction.java
@@ -0,0 +1,28 @@
+package org.example.bankingV1;
+
+public class Transaction<T extends Account<String>> {
+
+	private T account;
+	private double amount;
+	private boolean isDeposit;
+
+	public Transaction(T account, double amount, boolean isDeposit) {
+		this.account = account;
+		this.amount = amount;
+		this.isDeposit = isDeposit;
+	}
+
+	public void execute() {
+		if(isDeposit)  account.deposit(amount);
+		else  account.withdraw(amount);
+	}
+
+	@Override
+	public String toString() {
+		return "Transaction{" +
+			"account=" + account +
+			", amount=" + amount +
+			", isDeposit=" + isDeposit +
+			'}';
+	}
+}
